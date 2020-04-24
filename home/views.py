a381b89ab785c9fe0ff0e873be5e4ba318e50a9f
gurkan <gurkankirmaci@gmail.com>
2020-04-24 04:20:05 +0300

    contact form/iletişim formu  eklendi

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import  HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -21,7 +22,26 @@ def referanslar(request):
     return render(request, 'referanslar.html', context)
 
 
+
 def iletisim(request):
-    setting = Setting.objects.get(pk=1)
-    context = {'setting': setting,'page': 'iletisim' }
-    return render(request, 'iletisim.html', context)
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data=ContactFormMessage() #model ile baglanti kur
+            data.name = form.cleaned_data['name'] #formdan bilgi al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.messages= form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save() #veritabanına kaydet
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir,Teşekkür ederiz")
+            return HttpResponseRedirect ('/iletisim')
+
+    setting= Setting.objects.get(pk=1)
+    form= ContactFormu()
+    context={'setting':setting,'form':form}
+    return render(request, "iletisim.html",context)
+
+
+
