9b451c5cb176227870109e3b580f219cd27bb97f
gurkan <gurkankirmaci@gmail.com>
2020-05-03 22:43:09 +0300

    alt kategori menü ayarlandı...

--- product/admin.py
+++ product/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.utils.html import format_html
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 from product.models import Category, Product,Images
 
 class ProductImageInline(admin.TabularInline):
     model=Images
     extra=3
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(MPTTModelAdmin):
     list_display = ['title','status' ]
     list_filter = ['status']
 
@@ -21,6 +23,43 @@ class ProductAdmin(admin.ModelAdmin):
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['title', 'product','image_tag']
     readonly_fields = ('image_tag',)
-admin.site.register(Category,CategoryAdmin)
+
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_products_count', 'related_products_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = Category.objects.add_related_count(
+                qs,
+                Product,
+                'category',
+                'products_cumulative_count',
+                cumulative=True)
+
+        # Add non cumulative product count
+        qs = Category.objects.add_related_count(qs,
+                 Product,
+                 'category',
+                 'products_count',
+                 cumulative=False)
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+        related_product_count.short_description = 'Related products (for this specific category)'
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+    related_products_cumulative_count.short_description = 'Related products (in tree)'
+
+
+
+admin.site.register(Category,CategoryAdmin2)
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Images,ImageAdmin)
+
